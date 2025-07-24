a352d8588323d175fca05825417421f5e75671ce
aka06nksha <akankshacirigiri@gmail.com>
2025-07-24 21:56:23 +0530

    Binary Serach using Recursion

--- BinarySearchRecursion.java
+++ BinarySearchRecursion.java
@@ -1,8 +1,8 @@
-public class RotatedBinarySearch {
+public class BinarySearch {
     public static void main(String[] args) {
         int[] arr={1,2,3,4,5,6};
         
-        System.out.println(BinarySearch(arr,0,arr.length-1,6));
+        System.out.println(BinarySearch(arr,0,arr.length-1,7));
         
     
 }
@@ -13,10 +13,10 @@ public static int BinarySearch(int[] arr,int s,int e,int tar){
             return mid;
          }
          else if(arr[mid]>tar){
-            e=mid-1;
+            return BinarySearch(arr, s,mid-1,tar);
          }
          else{
-            s=mid+1;
+            return BinarySearch(arr, mid+1, e, tar);
          }
 }
 return -1;
